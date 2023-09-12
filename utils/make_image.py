014a2dfd48865e661ace51a1bf0937b210e01df6
CaixyPromise <caixypromised@gmail.com>
2023-09-12 13:36:52 +0800

    Update .gitignore

--- utils/make_image.py
+++
@@ -1,8 +0,0 @@
-from  PIL import Image, ImageTk
-
-
-def make_image(file, size = (33, 22)):
-    image = Image.open(file)
-    image = image.resize(size)
-    photo = ImageTk.PhotoImage(image)
-    return photo
